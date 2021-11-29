a96a70e70882d54b2ac12c0652644010e04a1571
Joachim Lengacher <joachim.lengacher@collaboration-factory.de>
2021-11-29 16:09:03 +0100

    Moved cplace-admin-extensions out of project again.

---
+++ src/test/java/cf/cplace/admin/CplaceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package cf.cplace.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CplaceAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
